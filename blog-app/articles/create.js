6137ca8a710ce639a1e54786d05492013a9059d8
Lukasz Malucha <lucasmalucha@gmail.com>
2020-07-02 18:50:03 +0100

    yml

--- blog-app/articles/create.js
+++ blog-app/articles/create.js
@@ -1,18 +1,37 @@
 'use strict';
 
-module.exports.handler = async (event, context) => {
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid');
 
-  const data = JSON.parse(event.body);
+module.exports.handler = async (event, context) => {
+  const data = event;
   if (data.text && typeof data.text !== 'string') {
     console.error('Validation Failed');
     return;
+  }
+
+  const params = {
+    TableName: 'BlogTable',
+    Item: {
+      article_id: uuid.v1(),
+      text: data.text
+    },
+  };
+  const putCallback = (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(new Error("Couldn't save the record"));
+      return;
     }
-    console.log(data.text);
     const response = {
       statusCode: 200,
-      body: JSON.stringify({
-        message: 'Created Article.'
-      }),
+      body: JSON.stringify(result),
     };
-    return response;
+    console.log(response)
+
+  }
+
+  dynamo.put(params, putCallback);
+
 };
--- readme.md
+++ readme.md
@@ -14,9 +14,17 @@ sls invoke -f hello
 ##### LOCAL
 sls invoke local -f hello
 
+#### INVOKE WITH JSON
+
+sls invoke local -f createArticle -p articles/event.json
+
 
 ##### RETRIEVE LOGS
 
 sls logs -f hello
 
-##### RETRIEVE ENDPOINT
+##### CONNECT APP TO DYNAMODB
+
+npm init -y
+npm i --save aws-sdk
+npm i --save uuid
